72f59e27f67263f3b9f1056d2d48bc8fe02d1db9
sndstudy <sanada2054tech@gmail.com>
2018-05-08 21:26:46 +0900

    四則計算・変数・複数行コメント 追加

--- first-ruby.rb
+++ first-ruby.rb
@@ -3,4 +3,67 @@
 #######
 
 #改行文字\n
-print("Hello\nWorld")
+print("Hello\nWorld\n")
+
+#エスケープ
+print("\"\\\n")
+
+#シングルクォーテーション（そのまま解釈）
+print('Hello\nWorld\n')
+print("\n")
+
+#putsメソッド（最後に改行する）
+#2つ渡すとそれぞれで改行する
+puts "Hello","World"
+
+##pメソッド（数値か文字か判別したいときに使える）
+##実行時の様子を確認するときに向いている
+puts "100"
+puts 100
+p "100"
+p 100
+
+#日本語出力
+#文字化けすることがあるので文字コードの指定（マジックコメント）をする場合がある
+# 例) # encoding: Shift_JIS
+print("おはようございます。\n")
+
+#############
+# 数値の表示と計算
+#############
+
+#数値の表示
+puts(10)
+
+#足し算
+puts(10 + 20)
+
+#引き算
+puts(20 - 10)
+
+#掛け算
+puts(5 * 10)
+
+#割り算
+puts(100 / 25)
+
+#四則計算の優先順位
+puts(20 + 8 / 2)
+
+#sinメソッド
+puts(Math.sin(1))
+
+#平方根
+puts(Math.sqrt(100))
+
+#変数
+age = 25
+puts(age)
+
+#複数行コメント
+=begin
+puts("test")    
+puts("test2")
+=end
+
+puts("test3")
