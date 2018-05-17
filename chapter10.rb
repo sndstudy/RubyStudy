f7a472cf7e80a1999838f0d72f4879c3c1e17829
sndstudy <sanada2054tech@gmail.com>
2018-05-17 23:12:39 +0900

    例外処理の書き方 追加

---
+++ chapter10.rb
@@ -0,0 +1,21 @@
+#エラー処理と例外
+
+##例外処理の書き方
+# coding: utf-8
+
+begin
+	#ゼロ除算を発生させている
+	a = 3/0
+	
+rescue Exception => e
+	
+	#例外の種類
+	p e.class
+
+	#例外のメッセージ
+	p e.message
+
+	#例外が発生した箇所に関する情報
+	p e.backtrace
+
+end
