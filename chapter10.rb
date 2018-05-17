abc1aa7003017501bacbddd829f0ddebce1a21bb
sndstudy <sanada2054tech@gmail.com>
2018-05-18 00:21:14 +0900

    後処理追加

--- chapter10.rb
+++ chapter10.rb
@@ -6,6 +6,8 @@
 begin
 	#ゼロ除算を発生させている
 	a = 3/0
+
+	puts("error")
 	
 rescue Exception => e
 	
@@ -17,5 +19,8 @@ rescue Exception => e
 
 	#例外が発生した箇所に関する情報
 	p e.backtrace
+ensure
+	#後処理
+	puts("ensure")
 
 end
